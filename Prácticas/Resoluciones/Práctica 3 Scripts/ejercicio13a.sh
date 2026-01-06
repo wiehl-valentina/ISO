010c4278aaefaa26de6fbaeda21cfc1697e2331b
Valentina Wiehl <valenwiehl@gmail.com>
2026-01-06 16:24:49 -0300

    cleaning

--- Prácticas/Resoluciones/Práctica 3 Scripts/ejercicio13a.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# recorre e informa 
-for i in {1..100}; do
-    echo "$i | $(($i * $i))"
-done
--- Prácticas/Resoluciones/Práctica 3 Scripts/ejercicio17.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-echo "`ls | tr -d 'aA' | tr '[:upper:][:lower:]' '[:lower:][:upper:]'`"
